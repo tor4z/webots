307c6bc2c1c1a0000375abfa689ff9727e1b6ffb
tor4z <vwenjie@hotmail.com>
2025-10-06 11:33:14 +0800

    Setup webots controller pkg

---
+++ my_module_test/src/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <stb/stb_ds.h>
+#include <glm/glm.hpp>
+#include <glad/glad.h>
+#include <OIS/OIS.h>
+#include <assimp/config.h>
+#include <picoos.h>
+#include <QtCore/QVector>
+
+
+int main()
+{
+    std::cout << "Hello, World\n";
+    return 0;
+}
